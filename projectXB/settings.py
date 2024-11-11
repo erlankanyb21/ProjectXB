240521d0a70f740d8d5487a385c36a874bc9eb7d
erlankanybekov <kanybekov668@gmail.com>
2024-11-11 22:03:49 +0600

    let's test refresh

--- projectXB/settings.py
+++ projectXB/settings.py
@@ -60,7 +60,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  # Время жизни токена
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),  # Время жизни токена
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
